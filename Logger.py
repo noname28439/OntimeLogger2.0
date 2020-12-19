d1a937053470ce0cb776d4bb05326632f63f3218
noname28439 <dohle.leon@web.de>
2020-12-19 19:36:31 +0100

    finished Logger.py

--- Logger.py
+++ Logger.py
@@ -14,11 +14,11 @@ import webbrowser
 import random
 import os.path
 import json
-from win32gui import GetWindowText, GetForegroundWindow
 import win32gui
 import win32con
 import win32api
 import win32process
+import pywintypes
 
 def list_to_string(list):
     string = ""
@@ -31,8 +31,10 @@ LOG_FILE_PATH = "./TimeLogs"
 is_afk = False
 afk_timeout = 0
 afk_set_to = 30
-off_sec = 0
 inGame = False
+
+
+counter_list = {}
 
 
 
@@ -45,26 +47,30 @@ def calculate_current_file_name():
     return LOG_FILE_PATH+"/date@"+year+"-"+month+"-"+day+".json"
 
 
-#File Management on load
-if not os.path.isfile(calculate_current_file_name()):
-    cf = open(calculate_current_file_name(), "w+")
-    cf.close()
-    print("File just created, because it didn't exist!")
-
-with open(calculate_current_file_name(), "r") as file:
-    content = file.read().split("\n")
-    #if len(content) >= 2:
-    #    on_sec = int(content[0])
-    #    off_sec = int(content[1])
-    #else:
-    #    print("Error, file not formated!")
+def check_file_exists():
+    if not os.path.isfile(calculate_current_file_name()):
+        cf = open(calculate_current_file_name(), "w+")
+        cf.close()
+        print("File just created, because it didn't exist!")
 
 
+def file_save():
+    global counter_list
+    check_file_exists()
+    with open(calculate_current_file_name(), "w") as file:
+        json.dump(counter_list, file)
 
-counter_list = {}
+def file_load():
+    global counter_list
+    with open(calculate_current_file_name(), "r") as file:
+        data = json.load(file)
+        counter_list = data
+
+file_load()
 
 def listmanagement_add_time(id, time):
-    counter_list[id][0] = counter_list[id][0]+time
+    if id in counter_list:
+        counter_list[id][0] = counter_list[id][0]+time
 
 def listmanagement_set_name(id, window_name):
     counter_list[id][1] = window_name
@@ -91,10 +97,12 @@ def main_thread():
         if inGame:
             afk_timeout = afk_set_to
 
-        current_active_window_name = str(GetWindowText(GetForegroundWindow()))
-        current_active_window_id = GetForegroundWindow()
+        current_active_window_name = str(win32gui.GetWindowText(win32gui.GetForegroundWindow()))
+        current_active_window_id = win32gui.GetForegroundWindow()
         current_active_window_path = get_current_active_window_path()
 
+        if not "OFF-TIME" in counter_list:
+            listmanagement_add_process("OFF-TIME", "offlineTimes")
 
         if current_active_window_path in counter_list:
             listmanagement_set_name(current_active_window_path, current_active_window_name)
@@ -108,13 +116,13 @@ def main_thread():
             afk_timeout -= 1
         else:
             if not is_afk:                  #Wenn er noch nicht AFK war, dann ist er seit exakt 30 Sek. AFK und diese 30 Sek. werden abgezogen
-                off_sec += afk_set_to
+                listmanagement_add_time("OFF-TIME", afk_set_to)
                 listmanagement_add_time(current_active_window_path, -afk_set_to)
             is_afk = True
-            off_sec += 1
+            listmanagement_add_time("OFF-TIME", 1)
 
 
-        print("afk: "+str(afk_timeout) + " || off: "+str(off_sec))
+        print("afk: "+str(afk_timeout))
         print("List: "+str(counter_list))
 
         #print("[ON_SEC: "+str(on_sec)+" MIN: "+str(round((on_sec/60),2))+" HOUR: "+str(round((on_sec/60/60),2))+""+" || afkTimeout: "+str(afk_timeout)+"]", end="\n")
@@ -125,8 +133,8 @@ def writerThread():
     global on_sec, off_sec, year, month, day
     while True:
         time.sleep(10)
-        with open(calculate_current_file_name(), "w") as file:
-            json.dump(counter_list, file)
+        file_save()
+        file_load()
 
 
 
